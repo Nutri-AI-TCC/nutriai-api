b374184cdb76d06bb5c23ba889fd8e112ba3bbb5
EllenFontes <fontes.ellen21@gmail.com>
2025-07-05 14:29:32 -0300

    refactor: reorganization on the project package display

---
+++ src/test/java/com/nutriai/api/HomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutriai.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
